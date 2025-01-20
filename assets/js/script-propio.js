fa4c4f278b036612c9a43dd95ce1d336c82c115e
Repositorio Desarrollo y Diseño Web DuocUC Viña del Mar <109529102+ddwduocvina@users.noreply.github.com>
2025-01-20 10:41:40 -0300

    Update script-propio.js

--- assets/js/script-propio.js
+++ assets/js/script-propio.js
@@ -1,12 +1,26 @@
 // función ajax
 $.ajax({
-    url: 'texto.txt',
+    url: './texto.txt',
     type: 'get',
-    dataType: 'txt',
+    dataType: 'text',
     success: function(respuesta){
-        document.write(respuesta)
+        document.getElementById('resultado').innerText = respuesta
     },
-    error: function(error){
-        document.write(error)
+    error: function(){
+        document.getElementById('resultado').innerText = 'No estoy logrando encontrar el documento.'
     }
 });
+
+// función get 
+$.get({
+    url: './gato.html',
+    dataType: 'html',
+    success: function(respuesta){
+        document.getElementById('iframe').innerHTML = respuesta
+        console.log(respuesta.status)
+    },
+    error: function(error){
+        alert('Este es un error tipo ' + error.status)
+        console.log(error.status)
+    }
+})
